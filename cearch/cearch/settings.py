29f60f00d2e3aa9af7d007548dacbba33a3ae9bd
ArgenEsenov <argenesenov2002@gmail.com>
2024-06-14 16:22:10 +0600

    end project почти

--- cearch/cearch/settings.py
+++ cearch/cearch/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+#
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%3glo+*tk=s*14gg6-@v70)1(v#$a=ajf^^tqdq-2d+x06#pe'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- cearch/myapp/views.py
+++ cearch/myapp/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+import httpx
+import json
+import asyncio
 
-# Create your views here.
+
+async def fetch_api_data(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return response.json()
+
+
+api_urls = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/comments",
+    "https://jsonplaceholder.typicode.com/albums",
+    "https://jsonplaceholder.typicode.com/photos",
+    "https://jsonplaceholder.typicode.com/todos",
+    "https://jsonplaceholder.typicode.com/posts/1/comments",
+    "https://jsonplaceholder.typicode.com/albums/1/photos",
+    "https://jsonplaceholder.typicode.com/users/1/posts",
+    "https://jsonplaceholder.typicode.com/users/1/todos"
+
+]
+
+
+async def fetch_all_data(urls):
+    tasks = [fetch_api_data(url) for url in urls]
+    results = await asyncio.gather(*tasks)
+    data = {}
+    for url, result in zip(urls, results):
+        key = url.split('/')[-1] if url.split('/')[-1] else url.split('/')[-2]
+        data[key] = result
+    return data
+
+
+async def search_data(data, search_term):
+    results = {key: [] for key in data}
+    search_term = search_term.lower()
+    # is_personal_number = len(search_term) == 14 and search_term.isdigit()
+
+    for key, items in data.items():
+        for item in items:
+            if isinstance(item, dict):
+                # Поиск по id или фамилии (name)
+                if 'name' in item and search_term in item['name'].lower():
+                    results[key].append(item)
+                # elif is_personal_number and str(item.get('id', '')).lower() == search_term:
+                elif str(item.get('id', '')).lower() == search_term:
+                    results[key].append(item)
+    return results
+
+
+async def index(request):
+    return render(request, 'search/index.html')
+
+
+# Асинхронное представление для выполнения поиска
+async def search(request):
+    search_term = request.GET.get('q', '')
+
+    # Получаем данные из всех API асинхронно
+    data = await fetch_all_data(api_urls)
+    results = await search_data(data, search_term)
+    print("Search term:", search_term)
+    print("Search results:")
+    print(json.dumps(results, indent=4))
+
+    return render(request, 'search/results.html', {'results': results, 'search_term': search_term})
